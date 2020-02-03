5543aaa01a92120a8674601b859c4aeed56f1c47
osadasami <osadasami@gmail.com>
2020-02-03 09:51:07 +0500

    add component structure

---
+++ src/app/orginizer/orginizer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orginizer',
+  templateUrl: './orginizer.component.html',
+  styleUrls: ['./orginizer.component.scss']
+})
+export class OrginizerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
