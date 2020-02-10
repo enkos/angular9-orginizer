b16a79b9a97b95cc20bc51530b5a5210296fa440
osadasami <osadasami@gmail.com>
2020-02-10 09:51:07 +0500

    add task

--- src/app/orginizer/orginizer.component.ts
+++ src/app/orginizer/orginizer.component.ts
@@ -1,4 +1,8 @@
+import { TasksService, Task } from './../shared/tasks.service';
+import { DateService } from './../shared/date.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-orginizer',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrginizerComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  tasks: Task[] = []
+
+  constructor(public dateSerice: DateService, private tasksService: TasksService) { }
 
   ngOnInit(): void {
+    this.dateSerice.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required)
+    })
+  }
+
+  submit() {
+    const {title} = this.form.value
+
+    const task: Task = {
+      title,
+      date: this.dateSerice.date.value.format('DD-MM-YYYY')
+    }
+
+    this.tasksService.create(task).subscribe(task => {
+      this.form.reset()
+    }, err => {
+      console.error(err)
+    })
   }
 
 }
